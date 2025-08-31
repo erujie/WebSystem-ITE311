9c50690144c3427a998acba4d7e730b60cab2e1f
erujie <erjaydosdos@gmail.com>
2025-08-31 14:16:26 +0800

    Added Home controller, routes, and views for basic navigation

---
+++ app/Views/contact.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Contact | ITE311</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+
+<?php include('index.php'); ?> 
+
+<div class="container mt-5">
+  <h1>Contact</h1>
+  <p>Contact chu-chu-chu-chu.</p>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
--- app/Views/index.php
+++ app/Views/index.php
@@ -2,16 +2,15 @@
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-  <title>Laboratory Exercise 1 | CodeIgniter + Bootstrap</title>
+  <title>Home | ITE311 - CodeIgniter + Bootstrap</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
-
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container-sm">
-    <a class="navbar-brand" >ITE311 Lab 1</a>
+    <a class="navbar-brand">ITE311</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
       aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
@@ -19,24 +18,14 @@
 
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav ms-auto">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#Home">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="">About</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="">Objectives</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="">Materials</a>
-        </li>
+        <li class="nav-item"><a class="nav-link" href="<?= base_url('/') ?>">Home</a></li>
+        <li class="nav-item"><a class="nav-link" href="<?= base_url('about') ?>">About</a></li>
+        <li class="nav-item"><a class="nav-link" href="<?= base_url('contact') ?>">Contact</a></li>
       </ul>
     </div>
   </div>
 </nav>
 
-
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
