bc118a20b83a8bddbe4d960f2243792306173ac7
erujie <erjaydosdos@gmail.com>
2025-09-01 14:34:59 +0800

    added index.php in the baseURL

--- app/Views/index.php
+++ app/Views/index.php
@@ -18,13 +18,14 @@
 
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav ms-auto">
-        <li class="nav-item"><a class="nav-link" href="<?= base_url('/') ?>">Home</a></li>
+        <li class="nav-item"><a class="nav-link" href="<?= base_url('home') ?>">Home</a></li>
         <li class="nav-item"><a class="nav-link" href="<?= base_url('about') ?>">About</a></li>
         <li class="nav-item"><a class="nav-link" href="<?= base_url('contact') ?>">Contact</a></li>
       </ul>
     </div>
   </div>
 </nav>
+
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
