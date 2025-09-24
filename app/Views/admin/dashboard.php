faa6559869f8d80b48abb4c4b1051105c9def635
Your Name <erjaydosdos@gmail.com>
2025-09-24 08:10:12 +0800

    admin dashboard complete. reminder: add container

--- app/Views/admin/dashboard.php
+++ app/Views/admin/dashboard.php
@@ -4,25 +4,50 @@
 
     <div class="row">
         <div class="col-md-4">
-            <div class="card shadow">
+            <div class="card border-dark">
                 <div class="card-body">
                     <h5>Total Users</h5>
-                    <p><?= $totalUsers ?? 'N/A' ?></p>
+                    <p>N/A</p>
                 </div>
             </div>
         </div>
         <div class="col-md-4">
-            <div class="card shadow">
+            <div class="card border-dark">
                 <div class="card-body">
                     <h5>Total Courses</h5>
-                    <p><?= $totalCourses ?? 'N/A' ?></p>
+                    <p>N/A</p>
                 </div>
             </div>
         </div>
+        <div class="mb-4"></div>
+
     </div>
+    
+    <div></div>
+    <div class="card">
+        <div class="card-header">Recent Activity</div>
+        <div class="card-body">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>Date</th>
+                        <th>User</th> 
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td> - </td>
+                        <td> - </td>
+                        <td> - </td>
+                    </tr>
 
-    <a href="<?= base_url('logout') ?>" class="btn btn-danger mt-3">Logout</a>
-
-</div>
+                </tbody>
+                <tfoot>
+                    <th class="text-center">No recent activity</th>
+                </tfoot>
+            </table>
+        </div>
+    </div>
 
 <?= $this->include('templates/footer') ?>
